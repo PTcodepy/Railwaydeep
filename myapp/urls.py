d75701930c2f300413444d9208e5473d35c14d77
Joao <joaodcarvalho@gmail.com>
2023-12-16 23:33:22 +0000

    first commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 # myapp/urls.py
 from django.urls import path
-from .views import index
+from .views import index, personal
 
 urlpatterns = [
     path('', index, name='index'),
+    path('personal/', personal, name='personal'),
 ]
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,10 @@
 # myproject/urls.py
 from django.contrib import admin
 from django.urls import include, path
+from myapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('myapp/', include('myapp.urls')),
+    path('', index, name='default'),
 ]
